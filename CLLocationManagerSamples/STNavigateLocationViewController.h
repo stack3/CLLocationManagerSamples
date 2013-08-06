59ee5255a4482aba4752da289421b8aac0524797
EIMEI <miyamoto@stack3.net>
2013-08-06 11:26:33 +0900

    Add navigation sample code.

--- CLLocationManagerSamples/STNavigateLocationViewController.h
+++ CLLocationManagerSamples/STNavigateLocationViewController.h
@@ -7,7 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
 
-@interface STNavigateLocationViewController : UIViewController
+@interface STNavigateLocationViewController : UIViewController<CLLocationManagerDelegate>
 
 @end
--- CLLocationManagerSamples/STNavigateLocationViewController.m
+++ CLLocationManagerSamples/STNavigateLocationViewController.m
@@ -8,13 +8,18 @@
 
 #import "STNavigateLocationViewController.h"
 
-@implementation STNavigateLocationViewController
+@implementation STNavigateLocationViewController {
+    IBOutlet __weak MKMapView *_mapView;
+    IBOutlet __weak UILabel *_errorLabel;
+    __strong CLLocationManager *_locationManager;
+    __strong NSDate *_startUpdatingLocationAt;
+}
 
 - (id)init
 {
     self = [super initWithNibName:nil bundle:nil];
     if (self) {
-        // Custom initialization
+        self.title = @"Navigate Location";
     }
     return self;
 }
@@ -22,13 +27,68 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    
+    _errorLabel.textColor = [UIColor whiteColor];
+    _errorLabel.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.5];
+    _errorLabel.text = nil;
+    _errorLabel.hidden = YES;
+    
+    _locationManager = [[CLLocationManager alloc] init];
+    _locationManager.delegate = self;
 }
 
-- (void)didReceiveMemoryWarning
+- (void)viewDidUnload
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [super viewDidUnload];
+ 
+    [_locationManager stopUpdatingLocation]; // Just in case.
+    _locationManager.delegate = nil;
+    _locationManager = nil;
+}
+- (void)viewDidAppear:(BOOL)animated
+{
+    [_locationManager startUpdatingLocation];
+    _startUpdatingLocationAt = [NSDate date];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [_locationManager stopUpdatingLocation];
+    _startUpdatingLocationAt = nil;
+}
+
+#pragma mark - CLLocationManagerDelegate
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
+{
+    CLLocation *recentLocation = locations.lastObject;
+    if (recentLocation.timestamp.timeIntervalSince1970 < _startUpdatingLocationAt.timeIntervalSince1970) {
+        // Ignore old location.
+        return;
+    }
+    
+    [_mapView setCenterCoordinate:recentLocation.coordinate animated:YES];
+    
+    MKPointAnnotation *mapAnnotation = [[MKPointAnnotation alloc] init];
+    mapAnnotation.coordinate = recentLocation.coordinate;
+    [_mapView removeAnnotations:_mapView.annotations];
+    [_mapView addAnnotation:mapAnnotation];
+ 
+    _errorLabel.text = nil;
+    _errorLabel.hidden = YES;
+    
+    NSLog(@"Updated location:%f %f timestamp:%@", recentLocation.coordinate.latitude, recentLocation.coordinate.longitude, recentLocation.timestamp.description);
+}
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
+{
+    CLAuthorizationStatus status = [CLLocationManager authorizationStatus];
+    if (status == kCLAuthorizationStatusAuthorized) {
+        _errorLabel.text = NSLocalizedString(@"Failed to get your location.", nil);
+    } else {
+        _errorLabel.text = NSLocalizedString(@"Alert Location Service Disabled", nil);
+    }
+    _errorLabel.hidden = NO;
 }
 
 @end
